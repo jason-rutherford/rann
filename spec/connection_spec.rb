5a34140fbeb94d5742647fa6230ce95b91cecf6e
Jason Rutherford <jason.rutherford@plansource.com>
2016-01-07 22:38:06 -0800

    Removed connection#weight_by_factor test.

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -22,10 +22,4 @@ describe Connection do
     @c = Connection.new(@n1, @n2, 2.5)
     expect(@c.weight).to eq 2.5
   end
-
-  it 'can set new weight by a factor of it' do
-    @c = Connection.new(@n1, @n2, 0.5)
-    @c.weight_by_factor(3)
-    expect(@c.weight).to eq 1.5
-  end
 end
